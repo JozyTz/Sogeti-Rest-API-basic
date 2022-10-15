8f3744a09db147411b6ce6505fb50598130b027b
Jon Blanchard <86966288+JozyTz@users.noreply.github.com>
2022-10-15 10:39:36 -0500

    Initial Commit
    2 hours (ish) time

---
+++ tech-assessment/CSharp/Controllers/OrdersController.cs
@@ -0,0 +1,71 @@
+﻿using CSharp.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSharp.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class OrdersController : ControllerBase
+    {
+        private List<Order> ordersDb = new List<Order>(){
+            new Order(1, 1, "Cust1Order1"),
+            new Order(2, 2, "Cust2Order1"),
+            new Order(3, 3, "Cust3Order1"),
+            new Order(4, 1, "Cust1Order2"),
+            new Order(5, 2, "Cust2Order2"),
+            new Order(6, 3, "Cust3Order2"),
+        };
+
+        [HttpPost]
+        public List<Order> CreateOrder([FromBody] Order order)
+        {
+            Random rand = new Random();
+            order.orderId = rand.Next();
+            ordersDb.Add(order);
+            return ordersDb;
+        }
+
+        [HttpGet]
+        public List<Order> GetOrdersByCustomer([FromQuery] int custId)
+        {
+            List<Order> orders = new List<Order>();
+            foreach (Order order in ordersDb)
+            {
+                if (order.customerId == custId)
+                {
+                    orders.Add(order);
+                }
+            }
+            return orders;
+        }
+
+        [HttpGet("orders")]
+        public List<Order> GetAllOrders()
+        {
+            return ordersDb;
+        }
+
+        [HttpPut]
+        public List<Order> UpdateOrder([FromBody] Order ord)
+        {
+            int size = ordersDb.Count();
+            ordersDb.RemoveAll(order => order.orderId == ord.orderId);
+            if (ordersDb.Count() != size)
+            {
+                ordersDb.Add(ord);
+            }
+            return ordersDb;
+        }
+
+        [HttpDelete]
+        public List<Order> CancelOrder([FromQuery] int ordId)
+        {
+            ordersDb.RemoveAll(order => order.orderId == ordId);
+            return ordersDb;
+        }
+    }
+}
---
+++ tech-assessment/CSharp/Model/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharp.Model
+{
+    public class Order
+    {
+        public int orderId {get; set;}
+        public int customerId {get; set;}
+        public string orderName {get; set;}
+        public Order(){}
+        public Order(int orderId, int customerId, string orderName)
+        {
+            this.orderId = orderId;
+            this.customerId = customerId;
+            this.orderName = orderName;
+        }
+    }
+}
